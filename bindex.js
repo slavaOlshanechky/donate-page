85365f9425bdf8e1b332cfd4d76a1586c341b7fa
slavaOlshanechky <olshanechky@gmail.com>
2021-11-29 14:33:27 +0200

    merge with feature/user-access

--- bindex.js
+++ bindex.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-console.log('Hello World')
-
-const userAge = 35
-if (userAge >= 18) {
-    console.log('You are too old')
-=======
 console.log('Hello world')
 const age = 20
 
 if (age >= 18) {
     console.log('You are too old')
 
->>>>>>> feature/user-access
 } else {
     console.log('You are too young')
 }
